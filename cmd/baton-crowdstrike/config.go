060a102d5dd271d446bd72e96b0c29f581b8a079
Martin Albert <martin.albert.187@gmail.com>
2023-08-07 17:01:06 +0200

    Use hypens instead of underscore in CLI flags

--- cmd/baton-crowdstrike/config.go
+++ cmd/baton-crowdstrike/config.go
@@ -12,15 +12,15 @@ import (
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
 
-	ClientId     string `mapstructure:"crowdstrike_client_id"`
-	ClientSecret string `mapstructure:"crowdstrike_client_secret"`
+	ClientId     string `mapstructure:"crowdstrike-client-id"`
+	ClientSecret string `mapstructure:"crowdstrike-client-secret"`
 	Region       string `mapstructure:"region"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
 	if cfg.ClientId == "" || cfg.ClientSecret == "" {
-		return fmt.Errorf("crowdstrike_client_id and crowdstrike_client_secret must be provided")
+		return fmt.Errorf("crowdstrike-client-id and crowdstrike-client-secret must be provided")
 	}
 
 	return nil
@@ -28,7 +28,7 @@ func validateConfig(ctx context.Context, cfg *config) error {
 
 // cmdFlags sets the cmdFlags required for the connector.
 func cmdFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().String("crowdstrike_client_id", "", "CrowdStrike client ID used to generate the access token. ($BATON_CROWDSTRIKE_CLIENT_ID)")
-	cmd.PersistentFlags().String("crowdstrike_client_secret", "", "CrowdStrike client secret used to generate the access token. ($BATON_CROWDSTRIKE_CLIENT_SECRET)")
+	cmd.PersistentFlags().String("crowdstrike-client-id", "", "CrowdStrike client ID used to generate the access token. ($BATON_CROWDSTRIKE_CLIENT_ID)")
+	cmd.PersistentFlags().String("crowdstrike-client-secret", "", "CrowdStrike client secret used to generate the access token. ($BATON_CROWDSTRIKE_CLIENT_SECRET)")
 	cmd.PersistentFlags().String("region", "us-1", "CrowdStrike region to connect to. ($BATON_REGION)")
 }
