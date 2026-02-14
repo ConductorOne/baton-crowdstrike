23b423fbccbdbb5133a97b9018bee27288bcd40d
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-14 14:54:53 -0800

    Hide base-url field from hosted UI
    base-url is a dev/testing concern, not user-facing configuration.
    Mark it WithHidden(true) so it doesn't appear in the hosted UI.
    
    Good feedback from Geoff:
    https://github.com/ConductorOne/baton-trayai/pull/63#discussion_r2805885290

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -27,6 +27,7 @@ var (
 	BaseURLField = field.StringField(
 		"base-url",
 		field.WithDescription("Override the CrowdStrike API URL (for testing)"),
+		field.WithHidden(true),
 	)
 
 	// ConfigurationFields defines the external configuration required for the
