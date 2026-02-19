a29e61b8656747fb852ed2466ec03ee87bcdf434
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:01:13 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -28,6 +28,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the CrowdStrike API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	// ConfigurationFields defines the external configuration required for the
